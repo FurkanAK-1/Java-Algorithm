a226833112e7f29a65b8fc4354b844a6d6fe6b35
Furkan AK <furkan.ak@stu.fsm.edu.tr>
2024-08-09 13:29:48 +0300

    Update Commit

--- src/java_algorithm/Java_Algorithm.java
+++ src/java_algorithm/Java_Algorithm.java
@@ -11,7 +11,7 @@ public class Java_Algorithm {
 
 	public static void drawCharacterArray(String input) {
 
-		char letter12;
+		char letter;
 		char letter2;
 
 		for (int i = 0; i < input.length(); i++) {
